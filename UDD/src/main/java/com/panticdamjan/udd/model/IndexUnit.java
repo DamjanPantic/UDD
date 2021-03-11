543aed26c1c6f032b737302f811f981a905bd734
DamjanPantic <panticdamjan@gmail.com>
2021-03-11 19:45:18 +0100

    index changed

--- UDD/src/main/java/com/panticdamjan/udd/model/IndexUnit.java
+++ UDD/src/main/java/com/panticdamjan/udd/model/IndexUnit.java
@@ -14,24 +14,23 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @NoArgsConstructor
 @Getter
 @Setter
-@Document(indexName = IndexUnit.INDEX_NAME, type = IndexUnit.TYPE_NAME, shards = 1, replicas = 0)
+@Document(indexName = IndexUnit.INDEX_NAME, shards = 1, replicas = 0)
 public class IndexUnit {
 
-    public static final String INDEX_NAME = "digitallibrary";
-    public static final String TYPE_NAME = "book";
+    public static final String INDEX_NAME = "book";
 
     @Id
     @Field(type = FieldType.Keyword)
     private String filename;
-    @Field(type = FieldType.Text, analyzer = "serbian")
+    @Field(type = FieldType.Text, analyzer = "serbian", searchAnalyzer = "serbian")
     private String text;
-    @Field(type = FieldType.Text, analyzer = "serbian")
+    @Field(type = FieldType.Text, analyzer = "serbian", searchAnalyzer = "serbian")
     private String title;
-    @Field(type = FieldType.Text, analyzer = "serbian")
+    @Field(type = FieldType.Text, analyzer = "serbian", searchAnalyzer = "serbian")
     private String keywords;
-    @Field(type = FieldType.Text, analyzer = "serbian")
+    @Field(type = FieldType.Text, analyzer = "serbian", searchAnalyzer = "serbian")
     private String genres;
-    @Field(type = FieldType.Text, analyzer = "serbian")
+    @Field(type = FieldType.Text, analyzer = "serbian", searchAnalyzer = "serbian")
     private String writer;
 
 }
